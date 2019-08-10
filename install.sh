f6cfe2e5fe5b282f4aec3dbacc438e244d4680f4
Rene Krenn <rkrenn@phoenixctms.org>
2019-08-10 16:40:55 +0200

    add inquirydataexport.sh shortcut

--- install.sh
+++ install.sh
@@ -205,6 +205,9 @@ rm /ctsms/bulk-processor.tar.gz -f
 wget https://raw.githubusercontent.com/phoenixctms/install-debian/master/ecrfdataexport.sh -O /ctsms/ecrfdataexport.sh
 chown ctsms:ctsms /ctsms/ecrfdataexport.sh
 chmod 755 /ctsms/ecrfdataexport.sh
+wget https://raw.githubusercontent.com/phoenixctms/install-debian/master/inquirydataexport.sh -O /ctsms/inquirydataexport.sh
+chown ctsms:ctsms /ctsms/inquirydataexport.sh
+chmod 755 /ctsms/inquirydataexport.sh
 
 ###initialize phoenix
 sudo -u ctsms /ctsms/dbtool.sh -i -f
@@ -246,8 +249,10 @@ sudo -u ctsms /ctsms/dbtool.sh -cu -dlk my_department -dp "$DEPARTMENT_PASSWORD"
 sed -r -i "s|ctsmsrestapi_password.*|ctsmsrestapi_password = ${CRON_PASSWORD}|" /ctsms/bulk_processor/CTSMS/BulkProcessor/Projects/ETL/Criteria/config.cfg
 sed -r -i "s|ctsmsrestapi_password.*|ctsmsrestapi_password = ${CRON_PASSWORD}|" /ctsms/bulk_processor/CTSMS/BulkProcessor/Projects/ETL/Duplicates/config.cfg
 sed -r -i "s|ctsmsrestapi_password.*|ctsmsrestapi_password = ${CRON_PASSWORD}|" /ctsms/bulk_processor/CTSMS/BulkProcessor/Projects/ETL/EcrfExporter/config.cfg
+sed -r -i "s|ctsmsrestapi_password.*|ctsmsrestapi_password = ${CRON_PASSWORD}|" /ctsms/bulk_processor/CTSMS/BulkProcessor/Projects/ETL/InquiryExporter/config.cfg
 IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 sed -r -i "s|ctsms_base_uri.*|ctsms_base_uri: 'https://${IP}'|" /ctsms/bulk_processor/CTSMS/BulkProcessor/Projects/ETL/EcrfExporter/settings.yml
+sed -r -i "s|ctsms_base_uri.*|ctsms_base_uri: 'https://${IP}'|" /ctsms/bulk_processor/CTSMS/BulkProcessor/Projects/ETL/InquiryExporter/settings.yml
 
 ###create some default queries/reports
 cd /ctsms/bulk_processor/CTSMS/BulkProcessor/Projects/ETL/Criteria
