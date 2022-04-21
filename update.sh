551b115fa85549d4c86c896f1e3d5f61cb082870
Rene Krenn <rkrenn@phoenixctms.org>
2022-04-21 23:05:29 +0200

    redirect errors of install.sh and update.sh to write them to the logfile

--- update.sh
+++ update.sh
@@ -8,4 +8,4 @@ fi
 wget https://raw.githubusercontent.com/phoenixctms/install-debian/$TAG/upgrade.sh -O ~/upgrade.sh
 chmod 744 ~/upgrade.sh
 
-sudo ~/upgrade.sh "$@" | tee ~/upgrade.log
+sudo ~/upgrade.sh "$@" 2>&1 | tee ~/upgrade.log
